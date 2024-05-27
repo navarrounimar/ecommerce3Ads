8d803edf0bbd0856b33305187f1c7a1f9e3d27e9
navarrounimar <navarrounimar@gmail.com>
2024-05-27 20:32:30 -0300

    alteracao Usuario

--- Ecommerce3Ads/Model/Usuario.cs
+++ Ecommerce3Ads/Model/Usuario.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+
     }
 }
